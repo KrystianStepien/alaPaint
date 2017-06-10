1b026b7b8419176ba34986176230a40fe01fcc4d
Krystian Stępień <kryssteq@gmail.com>
2017-06-10 14:56:22 +0200

    Dodanie palety kolorów (nie działającej)

--- alaPaint/Form1.Designer.cs
+++ alaPaint/Form1.Designer.cs
@@ -28,12 +28,210 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.czarny = new System.Windows.Forms.PictureBox();
+            this.bialy = new System.Windows.Forms.PictureBox();
+            this.niebieski = new System.Windows.Forms.PictureBox();
+            this.czerwony = new System.Windows.Forms.PictureBox();
+            this.zolty = new System.Windows.Forms.PictureBox();
+            this.zielony = new System.Windows.Forms.PictureBox();
+            this.brozowy = new System.Windows.Forms.PictureBox();
+            this.rozowy = new System.Windows.Forms.PictureBox();
+            this.pomaranczowy = new System.Windows.Forms.PictureBox();
+            this.fioletowy = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.czarny)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bialy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.niebieski)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.czerwony)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zolty)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zielony)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.brozowy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rozowy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pomaranczowy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fioletowy)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.pomaranczowy);
+            this.panel1.Controls.Add(this.brozowy);
+            this.panel1.Controls.Add(this.fioletowy);
+            this.panel1.Controls.Add(this.zolty);
+            this.panel1.Controls.Add(this.rozowy);
+            this.panel1.Controls.Add(this.zielony);
+            this.panel1.Controls.Add(this.niebieski);
+            this.panel1.Controls.Add(this.bialy);
+            this.panel1.Controls.Add(this.czerwony);
+            this.panel1.Controls.Add(this.czarny);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(79, 537);
+            this.panel1.TabIndex = 0;
+            // 
+            // czarny
+            // 
+            this.czarny.BackColor = System.Drawing.Color.Black;
+            this.czarny.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.czarny.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.czarny.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.czarny.Location = new System.Drawing.Point(3, 3);
+            this.czarny.Name = "czarny";
+            this.czarny.Size = new System.Drawing.Size(31, 32);
+            this.czarny.TabIndex = 0;
+            this.czarny.TabStop = false;
+            // 
+            // bialy
+            // 
+            this.bialy.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.bialy.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.bialy.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bialy.Location = new System.Drawing.Point(40, 3);
+            this.bialy.Name = "bialy";
+            this.bialy.Size = new System.Drawing.Size(33, 32);
+            this.bialy.TabIndex = 1;
+            this.bialy.TabStop = false;
+            // 
+            // niebieski
+            // 
+            this.niebieski.BackColor = System.Drawing.Color.Blue;
+            this.niebieski.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.niebieski.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.niebieski.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.niebieski.Location = new System.Drawing.Point(40, 41);
+            this.niebieski.Name = "niebieski";
+            this.niebieski.Size = new System.Drawing.Size(33, 32);
+            this.niebieski.TabIndex = 3;
+            this.niebieski.TabStop = false;
+            // 
+            // czerwony
+            // 
+            this.czerwony.BackColor = System.Drawing.Color.Red;
+            this.czerwony.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.czerwony.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.czerwony.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.czerwony.Location = new System.Drawing.Point(3, 41);
+            this.czerwony.Name = "czerwony";
+            this.czerwony.Size = new System.Drawing.Size(31, 32);
+            this.czerwony.TabIndex = 2;
+            this.czerwony.TabStop = false;
+            // 
+            // zolty
+            // 
+            this.zolty.BackColor = System.Drawing.Color.Yellow;
+            this.zolty.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.zolty.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.zolty.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.zolty.Location = new System.Drawing.Point(40, 79);
+            this.zolty.Name = "zolty";
+            this.zolty.Size = new System.Drawing.Size(33, 32);
+            this.zolty.TabIndex = 5;
+            this.zolty.TabStop = false;
+            // 
+            // zielony
+            // 
+            this.zielony.BackColor = System.Drawing.Color.Green;
+            this.zielony.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.zielony.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.zielony.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.zielony.Location = new System.Drawing.Point(3, 79);
+            this.zielony.Name = "zielony";
+            this.zielony.Size = new System.Drawing.Size(31, 32);
+            this.zielony.TabIndex = 4;
+            this.zielony.TabStop = false;
+            // 
+            // brozowy
+            // 
+            this.brozowy.BackColor = System.Drawing.Color.Brown;
+            this.brozowy.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.brozowy.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.brozowy.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.brozowy.Location = new System.Drawing.Point(40, 117);
+            this.brozowy.Name = "brozowy";
+            this.brozowy.Size = new System.Drawing.Size(33, 32);
+            this.brozowy.TabIndex = 3;
+            this.brozowy.TabStop = false;
+            // 
+            // rozowy
+            // 
+            this.rozowy.BackColor = System.Drawing.Color.Pink;
+            this.rozowy.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.rozowy.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rozowy.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rozowy.Location = new System.Drawing.Point(3, 117);
+            this.rozowy.Name = "rozowy";
+            this.rozowy.Size = new System.Drawing.Size(31, 32);
+            this.rozowy.TabIndex = 2;
+            this.rozowy.TabStop = false;
+            // 
+            // pomaranczowy
+            // 
+            this.pomaranczowy.BackColor = System.Drawing.Color.Orange;
+            this.pomaranczowy.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pomaranczowy.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pomaranczowy.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pomaranczowy.Location = new System.Drawing.Point(40, 155);
+            this.pomaranczowy.Name = "pomaranczowy";
+            this.pomaranczowy.Size = new System.Drawing.Size(33, 32);
+            this.pomaranczowy.TabIndex = 3;
+            this.pomaranczowy.TabStop = false;
+            // 
+            // fioletowy
+            // 
+            this.fioletowy.BackColor = System.Drawing.Color.Purple;
+            this.fioletowy.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.fioletowy.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fioletowy.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.fioletowy.Location = new System.Drawing.Point(3, 155);
+            this.fioletowy.Name = "fioletowy";
+            this.fioletowy.Size = new System.Drawing.Size(31, 32);
+            this.fioletowy.TabIndex = 2;
+            this.fioletowy.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.czarny)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bialy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.niebieski)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.czerwony)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zolty)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zielony)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.brozowy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rozowy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pomaranczowy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fioletowy)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pomaranczowy;
+        private System.Windows.Forms.PictureBox brozowy;
+        private System.Windows.Forms.PictureBox fioletowy;
+        private System.Windows.Forms.PictureBox zolty;
+        private System.Windows.Forms.PictureBox rozowy;
+        private System.Windows.Forms.PictureBox zielony;
+        private System.Windows.Forms.PictureBox niebieski;
+        private System.Windows.Forms.PictureBox bialy;
+        private System.Windows.Forms.PictureBox czerwony;
+        private System.Windows.Forms.PictureBox czarny;
     }
 }
 
