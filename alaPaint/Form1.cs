73654056be6abaf439d687e2a0938bf3a0c2328e
Krystian Stępień <kryssteq@gmail.com>
2017-06-12 22:00:57 +0200

    - dodano komunikat o obowiązku wyboru grubości ołówka do każdego koloru (wynika błąd po zmianie z gumki na ołówek że grubość ołówka była większa)

--- alaPaint/Form1.cs
+++ alaPaint/Form1.cs
@@ -39,63 +39,153 @@ namespace alaPaint
         #region Kolory
         private void czerwony_Click(object sender, EventArgs e)
         {
-            p.Color = czerwony.BackColor;
-            wybrany.BackColor = czerwony.BackColor;
+            if (rg.Checked)
+            {
+                MessageBox.Show("Proszę wybrać grubość ołówka", "Błąd grubości ołówka", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                p.Color = gumka.BackColor;
+                wybrany.BackColor = gumka.BackColor;
+            }
+            else
+            {
+                p.Color = czerwony.BackColor;
+                wybrany.BackColor = czerwony.BackColor;
+            }
 
         }
 
         private void niebieski_Click(object sender, EventArgs e)
         {
-            p.Color = niebieski.BackColor;
-            wybrany.BackColor = niebieski.BackColor;
+            if (rg.Checked)
+            {
+                MessageBox.Show("Proszę wybrać grubość ołówka", "Błąd grubości ołówka", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                p.Color = gumka.BackColor;
+                wybrany.BackColor = gumka.BackColor;
+            }
+            else
+            {
+                p.Color = niebieski.BackColor;
+                wybrany.BackColor = niebieski.BackColor;
+            }
         }
 
         private void zielony_Click(object sender, EventArgs e)
         {
-            p.Color = zielony.BackColor;
-            wybrany.BackColor = zielony.BackColor;
+            if (rg.Checked)
+            {
+                MessageBox.Show("Proszę wybrać grubość ołówka", "Błąd grubości ołówka", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                p.Color = gumka.BackColor;
+                wybrany.BackColor = gumka.BackColor;
+            }
+            else
+            {
+                p.Color = zielony.BackColor;
+                wybrany.BackColor = zielony.BackColor;
+            }
         }
 
         private void zolty_Click(object sender, EventArgs e)
         {
-            p.Color = zielony.BackColor;
-            wybrany.BackColor = zielony.BackColor;
+            if (rg.Checked)
+            {
+                MessageBox.Show("Proszę wybrać grubość ołówka", "Błąd grubości ołówka", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                p.Color = gumka.BackColor;
+                wybrany.BackColor = gumka.BackColor;
+            }
+            else
+            {
+                p.Color = zielony.BackColor;
+                wybrany.BackColor = zielony.BackColor;
+            }
         }
 
         private void rozowy_Click(object sender, EventArgs e)
         {
-            p.Color = rozowy.BackColor;
-            wybrany.BackColor = rozowy.BackColor;
+            if (rg.Checked)
+            {
+                MessageBox.Show("Proszę wybrać grubość ołówka", "Błąd grubości ołówka", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                p.Color = gumka.BackColor;
+                wybrany.BackColor = gumka.BackColor;
+            }
+            else
+            {
+                p.Color = rozowy.BackColor;
+                wybrany.BackColor = rozowy.BackColor;
+            }
         }
 
         private void brozowy_Click(object sender, EventArgs e)
         {
-            p.Color = brozowy.BackColor;
-            wybrany.BackColor = brozowy.BackColor;
+            if (rg.Checked)
+            {
+                MessageBox.Show("Proszę wybrać grubość ołówka", "Błąd grubości ołówka", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                p.Color = gumka.BackColor;
+                wybrany.BackColor = gumka.BackColor;
+            }
+            else
+            {
+                p.Color = brozowy.BackColor;
+                wybrany.BackColor = brozowy.BackColor;
+            }
         }
 
         private void pomaranczowy_Click(object sender, EventArgs e)
         {
-            p.Color = pomaranczowy.BackColor;
-            wybrany.BackColor = pomaranczowy.BackColor;
+            if (rg.Checked)
+            {
+                MessageBox.Show("Proszę wybrać grubość ołówka", "Błąd grubości ołówka", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                p.Color = gumka.BackColor;
+                wybrany.BackColor = gumka.BackColor;
+            }
+            else
+            {
+                p.Color = pomaranczowy.BackColor;
+                wybrany.BackColor = pomaranczowy.BackColor;
+            }
         }
 
         private void fioletowy_Click(object sender, EventArgs e)
         {
-            p.Color = fioletowy.BackColor;
-            wybrany.BackColor = fioletowy.BackColor;
+            if (rg.Checked)
+            {
+                MessageBox.Show("Proszę wybrać grubość ołówka", "Błąd grubości ołówka", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                p.Color = gumka.BackColor;
+                wybrany.BackColor = gumka.BackColor;
+            }
+            else
+            {
+                p.Color = fioletowy.BackColor;
+                wybrany.BackColor = fioletowy.BackColor;
+            }
         }
 
         private void bialy_Click(object sender, EventArgs e)
         {
-            p.Color = bialy.BackColor;
-            wybrany.BackColor = bialy.BackColor;
+            if (rg.Checked)
+            {
+                MessageBox.Show("Proszę wybrać grubość ołówka", "Błąd grubości ołówka", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                p.Color = gumka.BackColor;
+                wybrany.BackColor = gumka.BackColor;
+            }
+            else
+            {
+                p.Color = bialy.BackColor;
+                wybrany.BackColor = bialy.BackColor;
+            }
         }
 
         private void czarny_Click(object sender, EventArgs e)
         {
-            p.Color = czarny.BackColor;
-            wybrany.BackColor = czarny.BackColor;
+            if (rg.Checked)
+            {
+                MessageBox.Show("Proszę wybrać grubość ołówka", "Błąd grubości ołówka", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                p.Color = gumka.BackColor;
+                wybrany.BackColor = gumka.BackColor;
+            }
+            else
+            {
+                p.Color = czarny.BackColor;
+                wybrany.BackColor = czarny.BackColor;
+            }
         }
 
         #endregion Kolory
